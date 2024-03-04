b9d90cec02eaeafd5af0a6b8c11efd4ce5da5b1a
Laura Tsai <161272731+lauratsai59@users.noreply.github.com>
2024-03-03 22:00:58 -0500

    Add ui interface

--- main.js
+++ main.js
@@ -4,84 +4,83 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random()* max)];
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection, msg) {
     let result;
     if (playerSelection == computerSelection) {
         console.log(`Both have ${playerSelection} It is a tie.`);
-        alert(`Both have ${playerSelection}. It is a tie `);
+        msg.textContent = `Both have ${playerSelection}. It is a tie `;
         result = 0;
     } else if (computerSelection == 'rock') {
         if (playerSelection == 'scissors') {
             console.log(`Computer: ${computerSelection}, Player: ${playerSelection}. Computer wins`);
-            alert('Rock beats scissors. computer wins.')
+            msg.textContent = 'Rock beats scissors. Computer wins.';
             result = 1;
         } else {
             console.log(`Computer: ${computerSelection}, Player: ${playerSelection}. Player wins`);
-            alert('Paper Wraps rock. You win.')
+            msg.textContent = 'Paper Wraps rock. You win.';
             result = 2;
         }
     } else if (computerSelection == 'scissors') {
         if (playerSelection == 'paper') {
             console.log(`Computer: ${computerSelection}, Player: ${playerSelection}. Computer wins`);
-            alert('Scissors cuts paper. computer wins.')
+            msg.textContent = 'Scissors cuts paper. Computer wins.';
             result = 1;
         } else {
             console.log(`Computer: ${computerSelection}, Player: ${playerSelection}. Player wins`);
-            alert('Rock beats scissors. You win.')
+            msg.textContent = 'Rock beats scissors. You win.';
             result = 2;
         }
     } else if (computerSelection == 'paper') {
         if (playerSelection == 'rock') {
             console.log(`Computer: ${computerSelection}, Player: ${playerSelection}. Computer wins`);
-            alert('Paper wraps Rock. computer wins.')
+            msg.textContent = 'Paper wraps Rock. Computer wins.';
             result = 1;
         } else {
             console.log(`Computer: ${computerSelection}, Player: ${playerSelection}. Player wins`);
-            alert('Scissors cuts paper. You win.')
+            msg.textContent = 'Scissors cuts paper. You win.';
             result = 2;
         }
     }
     return result;
 }
-  
 
-function playGame() {
-    let playerSelection;
-    let computerScore = 0;
-    let playerScore = 0;
-    let score, message;
-
-    for (let i = 0; i < 5; i++) {
-        let computerSelection = getComputerChoice();
-        playerSelection = prompt("Your Choice(rock, paper, or scissors)");
-        if (playerSelection == null) {
-            console.log("Bye!");
-            break;
-        }
-        score = playRound(playerSelection.toLowerCase(), computerSelection);
-        if (score == 1) {
-            computerScore++;
-        } else if (score == 2) {
-            playerScore++;
-        }
+function addScore(score) {
+    if (score == 1) {
+        computerScore++;
+    } else if (score == 2) {
+        playerScore++;
     }
-
-    if (playerScore == computerScore) {
-        message = `Your Total Score: ${playerScore}. Computer Total Score: ${computerScore}. It is a tie.` ;
-    } else if (playerScore > computerScore) {    
-        message = `Your Total Score: ${playerScore}. Computer Total Score: ${computerScore}. You win.` ;
-    } else {
-        message =`Your Total Score: ${playerScore}. Computer Total Score: ${computerScore}. Computer wins.`;
+}
+   
+function displayScore(msg) {
+    msg.textContent = "";
+    if (playerScore >= 5 || computerScore >= 5) {
+        if (playerScore == computerScore) {
+            msg.textContent = `Your Total Score: ${playerScore}. Computer Total Score: ${computerScore}. It is a tie.` ;
+        } else if (playerScore > computerScore) {    
+            msg.textContent = `Your Total Score: ${playerScore}. Computer Total Score: ${computerScore}. You win.` ;
+        } else {
+            msg.textContent =`Your Total Score: ${playerScore}. Computer Total Score: ${computerScore}. Computer wins.`;
+        }
+        computerScore = 0;
+        playerScore = 0;
     }
-    alert(message);
 }
 
 
-playGame();
+const btns = document.querySelectorAll('.button');
+const msg = document.querySelector('.msg');
+const announcement = document.querySelector('.announcement');
+let computerScore = 0;
+let playerScore = 0;
 
-
-
-
-
-
-
+btns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        announcement.textContent = "";
+        msg.textContent = "";
+        let computerSelection = getComputerChoice();
+        let score = playRound(e.target.id, computerSelection, msg);
+        addScore(score);
+        displayScore(announcement);
+    })
+})
